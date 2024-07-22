915266ef47e39b83b5f091c1d9b276ed2553d77c
Lucas da Costa <lucas.costa2319@gmail.com>
2024-07-22 19:20:29 -0300

    Implementing the IPersonService Interface and adjusting the controller

--- 03_RestWithASPNETUdemy_ControllerEModel/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
+++ 03_RestWithASPNETUdemy_ControllerEModel/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET.Model;
+using RestWithASPNET.Services;
 using System.Globalization;
 
 namespace RestWithASPNET.Controllers
@@ -10,15 +12,47 @@ namespace RestWithASPNET.Controllers
 
         private readonly ILogger<PersonController> _logger;
 
-        public PersonController(ILogger<PersonController> logger)
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
         {
             _logger = logger;
+            _personService = personService;
         }
 
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Sum(string firstNumber, string secondNumber)
+        [HttpGet]
+        public IActionResult Get()
         {            
-            return BadRequest("Invalid Input");
+            return Ok(_personService.FindAll());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if(person == null) return NotFound();
+            return Ok(person);
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if(person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
         }
     }
 }
--- 03_RestWithASPNETUdemy_ControllerEModel/RestWithASPNET/RestWithASPNET/Program.cs
+++ 03_RestWithASPNETUdemy_ControllerEModel/RestWithASPNET/RestWithASPNET/Program.cs
@@ -1,8 +1,14 @@
+using RestWithASPNET.Services;
+using RestWithASPNET.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 
 var app = builder.Build();
 
