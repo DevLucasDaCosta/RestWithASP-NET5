3559e13c7ab8ee1193ed8dc2e620078000ed6826
Lucas da Costa <lucas.costa2319@gmail.com>
2024-07-23 22:21:25 -0300

    Creating and connecting to the database, defining database connection properties, and removing mocks

---
+++ 04_RestWithASPNETUdemy_CriacaoBancoDeDados/RestWithASPNET/RestWithASPNET/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Services;
+using RestWithASPNET.Services.Implementations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(9, 0, 0))));
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ 04_RestWithASPNETUdemy_CriacaoBancoDeDados/RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,46 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context) { 
+            _context = context;
+        }
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Test",
+                LastName = "Test",
+                Address = "São Paulo - SP",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
