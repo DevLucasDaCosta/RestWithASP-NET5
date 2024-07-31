462749f94b47a71cd1b69dfe0e385abe590a63e1
Lucas da Costa <lucas.costa2319@gmail.com>
2024-07-30 21:11:35 -0300

    Creating the base structure of the generic repository

---
+++ 09_RestWithASPNETUdemy_EstruturaBaseRepGenerico/RestWithASPNET/RestWithASPNET/Business/IPersonBusiness.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+        List<Person> FindAll();
+        Person FindById(long id);
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ 09_RestWithASPNETUdemy_EstruturaBaseRepGenerico/RestWithASPNET/RestWithASPNET/db/dataset/V2__Populate_table_person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Lucas', 'Male', 'da Costa'),
+	(2, 'São Paulo - Brasil', 'Clara', 'Female', 'Russo'),
+	(3, 'Vila Mariana - São Paulo', 'Rafael', 'Male', 'Russo'),
+	(4, 'Israel', 'Mohamed', 'Male', 'Bum'),
+	(5, 'USA', 'Nelson', 'Male', 'Mandela');
